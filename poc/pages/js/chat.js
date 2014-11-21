aa7c91a72b74530907ba98f870d74afa794ea303
oootyman <guilhem.drogue@orange.fr>
2014-11-21 12:55:46 +0100

    [poc] First work on chat page

---
+++ poc/pages/js/chat.js
@@ -0,0 +1,15 @@
+/* JavaScript Document*/
+
+$(
+	function()
+	{
+		$( '#headband' ).html("HEADER AGORA") ;
+		$( '#messages' ).html("MESSAGES") ;
+		$( '#wysiwyg' ).html("WYSIWYG") ;
+		$( '#notices' ).html("ANNONCES") ;
+		$( '#channels' ).html("CANAUX") ;
+		$( '#footer' ).html( "FOOTER AGORA" ) ;
+		$( '#menu' ).html( "LANGUES" ) ;
+	}
+);
+
