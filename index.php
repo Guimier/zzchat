5841b66293f006b3d038eb6f2f60c2fe4ad7e7d9
Guimier <lucien.guimier@laposte.net>
2014-11-29 13:19:16 +0100

    Integrated login page from drogue-poc

--- index.php
+++ index.php
@@ -24,16 +24,55 @@ function sendConfiguration()
 
 ?><!DOCTYPE html>
 <html>
-<head>
-	<title>Empty page</title>
-	<script src="web/js/configuration.js"></script>
-	<?php sendConfiguration() ; ?>
+	<head>
+		<!-- Meta information -->
+		<title></title>
+		<meta name="description" content="zz Chat Agora" />
+		<meta name="author" content="Guilhem Drogue, Lucien Guimier, Damien Teyssier" />
+		<link rel="shortcut icon" type="image/png" href="web/img/favicon.png" />
+	
+		<!-- Styles -->
+		<link rel="stylesheet" media="screen" href="web/css/chat.css" type="text/css" />
+	
+		<!-- Scripts -->
+	
+		<script src="web/js/configuration.js"></script>
+		<?php sendConfiguration() ; ?>
 
-	<script src="web/lib/jquery.js"></script>
-	<script src="web/js/ajax.js"></script>
-	<script src="web/js/languages.js"></script>
-</head>
-<body>
-</body>
+		<script src="web/lib/jquery.js"></script>
+		<script src="web/js/ajax.js"></script>
+		<script src="web/js/languages.js"></script>
+		<script src="web/js/chat.js"></script>
+	</head>
+	<body>
+        <!--Container-->
+        <div id="container">
+        	<!--Bandeau-->
+            <div id="headband"></div>
+            <!--Citations-->
+            <div id="quote"></div>
+            <!--Annonces-->
+            <div id="notices"></div>
+            <!--Chat-->
+            <div id="chat">
+ 				<!--Presents-->
+                <div id="presents"></div>
+            	<!--Messages-->
+                <div id="messages"></div>
+                <!--WYSIWYG-->
+                <div id="wysiwyg"></div>
+                <!--Canaux-->
+       		    <div id="channels"></div>
+            </div>
+            <!--Pour le centrage vertical-->
+            <div class="strut"></div>
+            <!--Formulaire-->
+            <div id="login"></div>
+            <!--Changeons de langue-->
+            <div id="menu"></div>
+            <!--Footer-->
+            <div id="footer"></div>
+      	</div>
+	</body>
 </html>
 
---
+++ web/js/chat.js
@@ -0,0 +1,104 @@
+/**
+ * @module control
+ */
+( function ( $ ) {
+	
+	/**
+	 * @method onLanguageChange
+	 */
+	function onLanguageChange()
+	{
+		var lang = $( this ).val() ;
+		languages.change( lang ) ;
+		configuration.setLocal( 'language', lang ) ;
+	}
+
+	function onLoginSubmit( evt )
+	{
+		evt.preventDefault() ;
+	
+		 // TODO
+	}
+
+	/**
+	 * On batit le formulaire pour la saisie du pseudonyme : method permet de spécifier la méthode utilisée pour passer les paramètres (post ou get), enctype donne le type du paramètre qui sera transmis, onsubmit réaliser une fonction à la soumission (içi vérifier la validité du pseudo).
+	 */
+	function $createForm()
+	{
+		return $form = $( '<form>' )
+			.attr( 'id', 'myForm' )
+			.submit( onLoginSubmit )
+			.append( $( '<label>' )
+				.attr( {
+					id: 'loginLabel',
+					'for': 'pseudo'
+				} )
+				.trText( "login.welcome" )
+			)
+			.append( '<br>' )
+			.append( $( '<input>' )
+				.attr( {
+					id: 'pseudo',
+					name: 'pseudo',
+					type: 'text'
+				} )
+			)
+			.append( '<br>' )
+			.append( $( '<input>' )
+				.attr( {
+					id: 'btn',
+					type: 'button'
+				} )
+				.trAttr("value", "login.validate")
+			)
+	}
+
+	function $createMenuLang()
+	{
+		var
+			$select = $( '<select>' ),
+			$option,
+			langList = configuration.get( 'languages' ),
+			current = languages.getCurrent() ;
+	
+		for ( var code in langList )
+		{
+			$option = $( '<option>' )
+				.text( langList[code] )
+				.val( code ) ;
+
+			if ( code === current )
+			{
+				$option.prop( 'selected', true ) ;
+			}
+
+			$select.append( $option ) ;
+		}
+	
+		return $( '<form>' )
+			.attr( {
+				id: 'listLang',
+				name: 'listLang'
+			} )
+			.html( $select
+				.attr( {
+					id: 'menuint',
+					name: 'menuint'
+				} )
+				.change( onLanguageChange )
+			) ;
+	}
+
+	$(
+		function()
+		{
+			/*Génération du choix des langues :*/
+			$( '#menu' ).html( $createMenuLang() ) ;
+			/*Génération : on remplace ce qui est désigné par l'ID dans le html par ce qui suit
+			Appel de $createForm pour batir le formulaire de login.*/
+			$( '#login' ).html( $createForm() ) ;
+			$( '#footer' ).trHtml( "web.footer" ) ;
+		}
+	);
+
+} ) ( jQuery ) ;
