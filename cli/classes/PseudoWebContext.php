22b4995eb6cf4d34983b021dcaf118c2a5263db3
Guimier <lucien.guimier@laposte.net>
2014-12-02 15:59:38 +0100

    [cli/ajax] finished first version

--- cli/classes/PseudoWebContext.php
+++ cli/classes/PseudoWebContext.php
@@ -8,8 +8,7 @@ class PseudoWebContext extends WebContext
 	private $trueContext ;
 	
 	/** Constructor.
-	 * @param array $getParams Array of GET parameters ($_GET).
-	 * @param array $postParams Array of POST parameters ($_POST).
+	 * @param CliContext $trueContext Real context.
 	 */
 	public function __construct( CliContext $trueContext )
 	{
@@ -36,42 +35,14 @@ class PseudoWebContext extends WebContext
 	/** Create and connect an user with the name in parameter.
 	 * 
 	 * @param string $userName The name of the user.
-	 * 
 	 */ 
-	public function connect( $userName )
-	{
-		$this->user = User::createUser( $userName ) ;
-	}
+	public function connect( $userName ) {}
 	
-	/**	Disconnect an user with the name in parameter.
-	 * 
-	 * @throw NotLoggedInUserException If no user is associated to the session.
-	 */ 
-	public function disconnect()
-	{
-		if ( $this->user === null )
-		{
-			throw new NotLoggedInUserException() ;
-		}
-		else
-		{
-			$this->user->isNowInactive() ;
-			$this->user = null ;
-		}
-	}
+	/**	Disconnect an user with the name in parameter. */ 
+	public function disconnect() {}
 	
 	/** Destructor. */
-	public function __destruct()
-	{
-		if ( $this->user === null )
-		{
-			unset ( $_SESSION['user-id'] ) ;
-		}
-		else
-		{
-			$_SESSION['user-id'] = $this->user->getId() ;
-		}
-	}
+	public function __destruct() {}
 	
 }
 
