b438a15b92b7d397cc596bc587054c63a5efc42a
Guimier <lucien.guimier@laposte.net>
2014-12-13 02:41:12 +0100

    [index.php] Fixed indentation, put notice logic out of HTML

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/common/init.php' ;
 header( 'Content-Type: text/html; charset=UTF-8' ) ;
 
+/** Build a call to “javacript:configuration.initialise()” with server configuration. */
 function sendConfiguration()
 {
 	echo '<script>' ;
@@ -20,7 +21,20 @@ function sendConfiguration()
 	
 	echo 'configuration.initialise( ' . JSON::encode( $jsConf ) . ' ) ;' ;
 	
-	echo '</script>' ;
+	echo "</script>\n" ;
+}
+
+/** Insert the notice if it has been defined. */
+function showNotice()
+{
+	$noticeFile = Configuration::getRootDir() . '/local/notice.html' ;
+	
+	if ( file_exists( $noticeFile ) )
+	{
+		echo '<div id="notices">' . file_get_contents( $noticeFile ) . "</div>" ;
+	}
+	
+	echo "\n" ;
 }
 
 ?><!DOCTYPE html>
@@ -53,69 +67,58 @@ function sendConfiguration()
 	
 		<script src="web/js/configuration.js"></script>
 		<?php sendConfiguration() ; ?>
-
 		<script src="web/lib/jquery.js"></script>
 		<script src="web/js/ajax.js"></script>
 		<script src="web/js/languages.js"></script>
 		<script src="web/js/chat.js"></script>
 	</head>
 	<body>
-        <!--Container-->
-        <div id="container">
-        	<!--Bandeau-->
-            <div id="headband">
+		<!--Container-->
+		<div id="container">
+			<!--Bandeau-->
+			<div id="headband">
 				<div id="agora"></div>
-                <!--Citations-->
-                <div id="quoteBlock">
+				<!--Citations-->
+				<div id="quoteBlock">
 					<div id="commands">
 						<span id="hello"></span>
 						<input id="disconnect" type="button">
 						<!--Changeons de langue-->
-						<select id="menuint"></select>	
+						<select id="menuint"></select>
 					</div>
 					<div id="quote"></div>
 					<div id="author"></div>
 				</div>
-            </div>
-            <div id="container2">
-<?php
-
-$noticeFile = Configuration::getRootDir() . '/local/notice.html' ;
-
-if ( file_exists( $noticeFile ) )
-{
-	echo '<div id="notices">' ;
-	echo file_get_contents( $noticeFile ) ;
-	echo '</div>' ;
-}
-?>
-                <!--Chat-->
-                <div id="chat">
-                	<!--Canaux-->
-                    <div id="channels-list"></div>
-                    <div id="channels">
-			<div class="channel currentChannel">
-				<div class="channelCore">
-					<!--Messages-->
-					<div class="messages"></div>
-					<!--WYSIWYG-->
-					<div class="wysiwyg"></div>
-				</div>
-				<!--Presents-->
-				<div class="presents"></div>
+			</div>
+			<div id="container2">
+				<?php showNotice() ; ?>
+				<!--Chat-->
+				<div id="chat">
+					<!--Canaux-->
+					<div id="channels-list"></div>
+					<div id="channels">
+						<div class="channel currentChannel">
+							<div class="channelCore">
+								<!--Messages-->
+								<div class="messages"></div>
+								<!--WYSIWYG-->
+								<div class="wysiwyg"></div>
+							</div>
+							<!--Presents-->
+							<div class="presents"></div>
+						</div>
+					</div>
 				</div>
 			</div>
-                </div>
-            </div>
-            <!--Pour le centrage vertical-->
-            <div class="strut"></div>
-            <!--Formulaire-->
-            <div id="login">
-            	<span id="nojs"><?php echo Context::getCanonical()->getMessage( 'web.nojs' ) ; ?></span>
-            </div>
-            <!--Footer-->
-            <div id="footer"></div>
-      	</div>
+			<!--Pour le centrage vertical-->
+			<div class="strut"></div>
+			<!--Formulaire-->
+			<div id="login">
+				<span id="nojs"><?php echo Context::getCanonical()->getMessage( 'web.nojs' ) ; ?></span>
+			</div>
+			<!--Footer-->
+			<div id="footer"></div>
+		</div>
 	</body>
 </html>
 
