9a6bcadbb78b539797392d09e1ae2e3f19067032
Guimier <lucien.guimier@laposte.net>
2014-12-17 01:26:03 +0100

    [tests] Quotations : full coverage

--- tests/QuotationsCommand/QuotationsCommandTest.php
+++ tests/QuotationsCommand/QuotationsCommandTest.php
@@ -6,31 +6,85 @@ require_once dirname( __DIR__ ) . '/ClassTester.php' ;
 /** Test for QuotationsCommand. */
 class QuotationsCommandTest extends ClassTester
 {
-	
+
+/*----- add -----*/
+
 	/** @expectedException CliMissingParameterException */
-	public function testAdditionMissingTestParameter()
+	public function testAddMissingTestParameter()
 	{
 		$this->runCommand( 'QuotationsCommand', 'add' ) ;
 	}
 	
-	public function testAdditionWithAuthor()
+	public function testAddWithAuthor()
 	{
 		$this->runCommand( 'QuotationsCommand', 'add', '--text=A text', '--author=An author' ) ;
 		$this->assertEquals(
 			Quotations::$calls,
 			array(
-				array( 'text' => 'A text', 'author' => 'An author' )
+				array( 'add', 'text' => 'A text', 'author' => 'An author' )
 			)
 		) ;
 	}
 	
-	public function testAdditionWithoutAuthor()
+	public function testAddWithoutAuthor()
 	{
 		$this->runCommand( 'QuotationsCommand', 'add', '--text=A text' ) ;
 		$this->assertEquals(
 			Quotations::$calls,
 			array(
-				array( 'text' => 'A text', 'author' => null )
+				array( 'add', 'text' => 'A text', 'author' => null )
+			)
+		) ;
+	}
+
+/*----- rm -----*/
+
+	/** @expectedException CliMissingParameterException */
+	public function testRmMissingTestParameter()
+	{
+		$this->runCommand( 'QuotationsCommand', 'rm' ) ;
+	}
+
+	public function testRm()
+	{
+		$this->runCommand( 'QuotationsCommand', 'rm', '--id=1' ) ;
+		$this->assertEquals(
+			Quotations::$calls,
+			array(
+				array( 'remove', 'id' => 1 )
+			)
+		) ;
+	}
+
+/*----- show -----*/
+
+	public function testShowAll()
+	{
+		$this->runCommand( 'QuotationsCommand', 'show' ) ;
+		$this->expectOutputString(
+<<<RAW
+0] “I am” — Me
+1] “You are” — Anonymous
+
+RAW
+		) ;
+	}
+
+	public function testShowOne()
+	{
+		$this->runCommand( 'QuotationsCommand', 'show', '--id=1' ) ;
+		
+		$this->expectOutputString(
+<<<RAW
+1] “I am” — Me
+
+RAW
+		) ;
+		
+		$this->assertEquals(
+			Quotations::$calls,
+			array(
+				array( 'get', 'id' => 1 )
 			)
 		) ;
 	}
--- tests/QuotationsCommand/placeholders.php
+++ tests/QuotationsCommand/placeholders.php
@@ -7,6 +7,25 @@ class Quotations
 	
 	public function add( $text, $author = null )
 	{
-		self::$calls[] = array( 'text' => $text, 'author' => $author ) ;
+		self::$calls[] = array( 'add', 'text' => $text, 'author' => $author ) ;
+	}
+	
+	public function remove( $id )
+	{
+		self::$calls[] = array( 'remove', 'id' => $id ) ;
+	}
+	
+	public function getAll()
+	{
+		return array(
+			array( 'text' => 'I am', 'author' => 'Me' ),
+			array( 'text' => 'You are', 'author' => null )
+		) ;
+	}
+	
+	public function get( $id )
+	{
+		self::$calls[] = array( 'get', 'id' => $id ) ;
+		return array( 'text' => 'I am', 'author' => 'Me' ) ;
 	}
 }
