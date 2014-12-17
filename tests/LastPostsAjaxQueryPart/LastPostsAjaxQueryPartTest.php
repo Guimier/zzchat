c230bb5ff0087a9cf657a39548ef3e24cadb11a5
teyssierdamien <teyssier_damien@yahoo.fr>
2014-12-17 21:37:11 +0100

    LastPostAjaxQueryPartTest done.

--- tests/LastPostsAjaxQueryPart/LastPostsAjaxQueryPartTest.php
+++ tests/LastPostsAjaxQueryPart/LastPostsAjaxQueryPartTest.php
@@ -6,12 +6,6 @@ require_once dirname( __DIR__ ) . '/ClassTester.php' ;
 /** Test for LastPostAjaxQueryPart. */
 class LastPostsAjaxQueryPartTest extends ClassTester
 {
-	/** @expectedException WebMissingParameterException */
-	public function testWithoutParameter()
-	{
-		$this->runAjax() ;
-	}
-
 	public function testWithoutChannelsidsParameter()
 	{
 		$res = $this->runAjax( array( 'p_from' => '10' ) ) ;
@@ -21,6 +15,34 @@ class LastPostsAjaxQueryPartTest extends ClassTester
 			'posts' => array()
 			)
 		) ; 
+	}
+	
+	/** @expectedException WebMissingParameterException */
+	public function testWithoutFromParameter()
+	{
+		$this->runAjax( array( 'p_channels' => '1,2,3' ) ) ;		
+	}
+	
+	public function testExecute()
+	{
+		$res = $this->runAjax( array( 'p_channels' => '1,2', 'p_from' => '10' ) ) ;
+		
+		$this->assertEquals(
+			$res,
+			array ( 
+				'date' => 42,
+				'posts' => array(
+					'1' => array(
+						array( 'id' => 1, 'owner' => array( 'id' => 18, 'name' => 'A user' ), 'date' => 43, 'content' => 'Bonjour1' ),
+						array( 'id' => 2, 'owner' => array( 'id' => 18, 'name' => 'A user' ), 'date' => 44, 'content' => 'Bonjour2' )
+					),
+					'2' => array(
+						array( 'id' => 1, 'owner' => array( 'id' => 18, 'name' => 'A user' ), 'date' => 43, 'content' => 'Bonjour1' ),
+						array( 'id' => 2, 'owner' => array( 'id' => 18, 'name' => 'A user' ), 'date' => 44, 'content' => 'Bonjour2' ),
+					)
+				)
+			)
+		) ;	
 	}
 	
 }
--- tests/LastPostsAjaxQueryPart/placeholders.php
+++ tests/LastPostsAjaxQueryPart/placeholders.php
@@ -2,18 +2,49 @@
 // @codeCoverageIgnoreStart
 
 class Channel
-{
-	public function getById( $id ) 
+{	
+	public static function getById( $id ) 
 	{
-		return new Channel ; 
+		return new Channel() ; 
 	}
-	public function LastPosts( $beginning )
+	
+	public function lastPosts( $from )
 	{
-		return array( "Premier", "Deuxième", "Dernier" ) ; 
+		return array( new Post( 1 ), new Post( 2 ) ) ; 
 	}
 }
 
 class Post
 {
 	
+	public function __construct( $id )
+	{
+		$this->postId = $id ;
+	}
+	
+	public function getid()
+	{
+		return $this->postId ;
+	}
+	
+	public function getOwner()
+	{
+		return new User() ;
+	}
+	
+	public function getDate()
+	{
+		return 42 + $this->postId ; 
+	}
+	
+	public function getContent()
+	{
+		return "Bonjour" . $this->postId ;
+	}
+}
+
+class User
+{
+	public function getId() { return 18 ; }
+	public function getName() { return 'A user' ; }
 }
